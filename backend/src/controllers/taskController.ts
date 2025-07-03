e8c765e73faf127f16ed3b2ebe5b734c5d328ed5
SebastianGaleano20 <sebascarp9@gmail.com>
2025-07-02 22:45:46 -0300

    taskController added - CRUD

---
+++ backend/src/controllers/taskController.ts
@@ -0,0 +1,10 @@
+import { Response, Request, NextFunction } from "express";
+import httpStatus from "../utils/httpStatus.js";
+
+export const taskController = () => {
+  const createTask = (req: Request, res: Response, next: NextFunction) => {};
+  const getTasks = (req: Request, res: Response, next: NextFunction) => {};
+  const editTask = (req: Request, res: Response, next: NextFunction) => {};
+  const deleteTask = (req: Request, res: Response, next: NextFunction) => {};
+  return { createTask, getTasks };
+};
---
+++ backend/src/utils/httpStatus.ts
@@ -0,0 +1,13 @@
+import type { HttpStatus } from "../types/utils/index.js";
+
+const httpStatus: HttpStatus = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  UNPROCESSABLE_ENTITY: 422,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+export default httpStatus;
