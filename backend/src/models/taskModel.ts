6ae27b1af53552c356c285047cad52b6677be172
SebastianGaleano20 <sebascarp9@gmail.com>
2025-07-04 12:57:44 -0300

    taskService completed

--- backend/src/models/taskModel.ts
+++ backend/src/models/taskModel.ts
@@ -1,6 +1,5 @@
-import Task from "../types";
 import { readData, writeData } from "../utils/dbManagment";
-
+import type Task from "../types/index.js";
 export const taskModel = () => {
   const getDataTasks = async () => {
     try {
@@ -31,10 +30,24 @@ export const taskModel = () => {
       throw new Error(error.message);
     }
   };
-
+  const deleteData = async (id: number) => {
+    try {
+      const tasks = await readData();
+      const task = tasks.find((task: Task) => task.id === id);
+      if (!task) {
+        throw new Error("Task not found");
+      }
+      const newTasks = tasks.filter((task: Task) => task.id !== id);
+      await writeData(newTasks);
+      return true;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  };
   return {
     getDataTasks,
     saveDataTask,
     getDataById,
+    deleteData,
   };
 };
---
+++ backend/src/services/taskService.ts
@@ -0,0 +1,47 @@
+import { taskModel } from "../models/taskModel";
+import type Task from "../types/index.js";
+
+export const taskService = () => {
+  const { getDataById, getDataTasks, saveDataTask, deleteData } = taskModel();
+
+  const getAllTasks = async () => {
+    try {
+      const tasks = await getDataTasks();
+      return tasks;
+    } catch (error) {
+      throw error;
+    }
+  };
+  const createTask = async (task: Task) => {
+    try {
+      const newTask = await saveDataTask(task);
+      return newTask;
+    } catch (error) {
+      throw error;
+    }
+  };
+  const updateTask = async (dataTask: Partial<Task>, taskId: number) => {
+    try {
+      const task = await getDataById(taskId);
+      if (!task) throw new Error("Task not found");
+      Object.assign(task, dataTask);
+      return task;
+    } catch (error) {
+      throw error;
+    }
+  };
+  const deleteTask = async (taskId: number) => {
+    try {
+      await deleteData(taskId);
+      return true;
+    } catch (error) {
+      throw error;
+    }
+  };
+  return {
+    getAllTasks,
+    createTask,
+    updateTask,
+    deleteTask,
+  };
+};
