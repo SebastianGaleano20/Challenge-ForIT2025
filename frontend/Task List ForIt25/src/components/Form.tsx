6ce465f30e98c0c34b4d5b9618b990597341d849
SebastianGaleano20 <sebascarp9@gmail.com>
2025-07-07 13:32:17 -0300

    fetch in form - POST METHOD completed

--- frontend/Task List ForIt25/src/components/Form.tsx
+++ frontend/Task List ForIt25/src/components/Form.tsx
@@ -1,8 +1,51 @@
 import "../styles/form.css";
+import React, { useState } from "react";
+import type { FormEvent } from "react";
+import type { FormData } from "../types/components";
+
 export default function Form() {
+  const [formData, setFormData] = useState<FormData>({
+    title: "",
+    description: "",
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleTextareaChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:2010/api/tasks/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log("Éxito:", data);
+    } catch (error) {
+      console.error("Error al enviar el formulario:", error);
+      throw new Error(`Error: ${error}`);
+    }
+  };
+
   return (
     <section className="form-container">
-      <form id="form" target="_blank" method="post">
+      <form id="form" onSubmit={handleSubmit} target="_blank" method="post">
         <label id="title" htmlFor="title">
           Título:
         </label>
@@ -12,17 +55,20 @@ export default function Form() {
           id="title"
           placeholder="Ingrese el título de la tarea por favor"
           required
+          value={formData.title}
+          onChange={handleInputChange}
         />
-        <label id="data" htmlFor="data">
+        <label id="description" htmlFor="description">
           Datos de la tarea:
         </label>
         <textarea
-          className="data"
-          name="data"
-          id="data"
+          name="description"
+          id="description"
           rows={8}
           placeholder="Ingrese la descripción de la tarea por favor"
           required
+          value={formData.description}
+          onChange={handleTextareaChange}
         ></textarea>
         <button type="submit" value="Enviar Datos">
           Crear tarea
