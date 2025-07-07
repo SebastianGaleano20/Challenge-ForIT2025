970dfcac34a35ebcf1da7cc532f963e806f69458
SebastianGaleano20 <sebascarp9@gmail.com>
2025-07-07 13:03:29 -0300

    Form styled

--- frontend/Task List ForIt25/src/components/Form.tsx
+++ frontend/Task List ForIt25/src/components/Form.tsx
@@ -3,11 +3,30 @@ export default function Form() {
   return (
     <section className="form-container">
       <form id="form" target="_blank" method="post">
-        <label htmlFor="title">Ingrese el título de la tarea: </label>
-        <input type="text" name="title" id="title" required />
-        <label htmlFor="data">Datos de la tarea:</label>
-        <textarea className="data" name="data" id="data" rows={8}></textarea>
-        <input type="submit" value="Enviar Datos" />
+        <label id="title" htmlFor="title">
+          Título:
+        </label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="Ingrese el título de la tarea por favor"
+          required
+        />
+        <label id="data" htmlFor="data">
+          Datos de la tarea:
+        </label>
+        <textarea
+          className="data"
+          name="data"
+          id="data"
+          rows={8}
+          placeholder="Ingrese la descripción de la tarea por favor"
+          required
+        ></textarea>
+        <button type="submit" value="Enviar Datos">
+          Crear tarea
+        </button>
       </form>
     </section>
   );
